7fe7e2310dcc0f039aedbc30e5a346e888dca52a
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-10-14 17:00:59 +0530

    fix mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import http from "http";
+import mongoose from "mongoose";
 
 const PORT = 5000;
+const MONGO_URL = "mongodb+srv://user:user@cluster0.5ybjl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const app = express();
 
 app.use(cors({ credentials: true }));
@@ -16,3 +19,14 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}/`);
 });
+
+mongoose.Promise = Promise;
+mongoose
+   .connect(MONGO_URL)
+   .then((conn) => {
+      console.log(`MongoDB connected: ${conn.connection.host}`);
+   })
+   .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+   });
+mongoose.connection.on("MongoDB connection error", (error: Error) => console.log(error));
