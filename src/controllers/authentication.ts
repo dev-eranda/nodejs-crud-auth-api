d1af68b1ab296d20226908e0acda9316aeb6dbcd
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-10-15 17:25:27 +0530

    fix login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -34,3 +34,39 @@ export const register = async (req: express.Request, res: express.Response): Pro
       res.sendStatus(500);
    }
 };
+
+export const login = async (req: express.Request, res: express.Response): Promise<void> => {
+   try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+         res.sendStatus(400);
+         return;
+      }
+
+      const user = await getUserByEmail(email).select("+authentication.salt +authentication.password");
+
+      if (!user) {
+         res.sendStatus(404);
+         return;
+      }
+
+      const expectedHash = authentication(user.authentication.salt, password);
+
+      if (user.authentication.password !== expectedHash) {
+         res.sendStatus(403);
+         return;
+      }
+
+      const salt = random();
+      user.authentication.sessionToken = authentication(salt, user._id.toString());
+      await user.save();
+
+      res.cookie("auth-cookie", user.authentication.sessionToken, { domain: "localhost", path: "/" });
+
+      res.status(200).json(user);
+   } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+   }
+};
