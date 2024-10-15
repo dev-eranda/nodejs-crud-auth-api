ff72351b4b0263665fa46d91997ffd071d87efe2
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-10-15 18:27:27 +0530

    fix getAllUsers

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -19,7 +19,7 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
 
       merge(req, { identity: existingUser });
 
-      return next();
+      next();
    } catch (error) {
       console.log(error);
       res.sendStatus(500);
