0ef833d0d481a9ea034389fd6d02be349e15affc
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-10-15 18:10:03 +0530

    fix middleware for 'isAuthenticated'

---
+++ src/middlewares/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { merge } from "lodash";
+import { getUserBySessionToken } from "../db/users";
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+   try {
+      const sessionToken = req.cookies["auth-cookie"];
+
+      if (!sessionToken) {
+         res.sendStatus(403);
+         return;
+      }
+
+      const existingUser = await getUserBySessionToken(sessionToken);
+      if (!existingUser) {
+         res.sendStatus(403);
+         return;
+      }
+
+      merge(req, { identity: existingUser });
+
+      return next();
+   } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+   }
+};
