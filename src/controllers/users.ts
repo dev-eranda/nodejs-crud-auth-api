830dbd40026fdd34d1e94dd31eae23f3f94d960a
ErandaMadusanka <emsamarasinghe@gmail.com>
2024-10-16 15:52:28 +0530

    fix update user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
    try {
@@ -19,6 +19,31 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
       const deleteUser = await deleteUserById(id);
 
       res.status(204).json(deleteUser);
+   } catch (error) {
+      console.log(error);
+      res.sendStatus(500);
+   }
+};
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+   try {
+      const { id } = req.params;
+      const { username } = req.body;
+
+      if (!username) {
+         res.sendStatus(400);
+         return;
+      }
+
+      const user = await getUserById(id);
+      if (!user) {
+         res.sendStatus(404);
+      }
+
+      user.username = username;
+      user.save();
+
+      res.status(200).json(user);
    } catch (error) {
       console.log(error);
       res.sendStatus(500);
